16b53a77c9e4baae89e5b5d8a02a14a8e80d3287
Roger Welsh <rjhwelsh@protonmail.ch>
2018-07-09 14:54:59 +1200

    parsync/Rsync.py (__init__): Added rsyncBin param and Defaults.

--- parsync/Rsync.py
+++ parsync/Rsync.py
@@ -2,9 +2,16 @@
 
 # Copyright (c) 2018 Roger Welsh <rjhwelsh@gmail.com>
 
+import parsync.FileSet as FileSet
+
 
 class Rsync:
-    def __init__(self, mainSet, ignoreSet, packageSet, rsyncArgs):
+    def __init__(self,
+                 mainSet=FileSet.FileSet(),
+                 ignoreSet=FileSet.FileSet(),
+                 packageSet=dict(),
+                 rsyncBin="",
+                 rsyncArgs=""):
         """
         This class provides an interface to rsync w/ filesets.
         """
@@ -12,3 +19,4 @@ class Rsync:
         self.ignoreSet = ignoreSet
         self.packageSet = packageSet
         self.rsyncArgs = rsyncArgs
+        self.rsyncBin = rsyncBin
