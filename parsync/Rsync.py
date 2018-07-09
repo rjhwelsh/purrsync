3987b53383b9e70579f9b887f4d15c97ad7c5cb4
Roger Welsh <rjhwelsh@protonmail.ch>
2018-07-09 15:17:22 +1200

    parsync/Rsync.py (rsyncMain): Added function.

--- parsync/Rsync.py
+++ parsync/Rsync.py
@@ -4,6 +4,7 @@
 
 import subprocess as sp
 import os
+import tempfile
 
 import parsync.FileSet as FileSet
 
@@ -30,3 +31,17 @@ class Rsync:
                       stdout=sp.PIPE) as proc:
             return proc.stdout.read().decode(
                 "utf-8").rstrip(os.linesep)
+    def rsyncMain(self):
+        """ Rsyncs main set. """
+        with tempfile.NamedTemporaryFile(mode='r') as syncCacheFile:
+
+            mainSet = self.mainSet.read()
+            ignoreSet = self.ignoreSet.read()
+            syncSet = mainSet - ignoreSet
+
+            syncFileSet = FileSet.FileSet(
+                setIter=syncSet,
+                filename=syncCacheFile.name)
+
+            syncFileSet.write()
+            return self.rsync(syncCacheFile.name)
