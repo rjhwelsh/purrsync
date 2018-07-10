5e87a4c0b7ac527d383d9f7a014c46738c6a6525
Roger Welsh <rjhwelsh@protonmail.ch>
2018-07-10 16:57:08 +1200

    setup.py : Added setup script.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2018 Roger Welsh <rjhwelsh@gmail.com>
+
+from setuptools import setup
+
+setup(
+    name='purrsync',
+    version='0.1.0',
+    packages=['purrsync'],
+    entry_points={
+        'console_scripts': [
+            'purrsync = purrsync.__main__:main'
+        ]
+    })
