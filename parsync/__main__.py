5d26bc19bb14dd35eed403e5d7ab88ccbf872879
Roger Welsh <rjhwelsh@protonmail.ch>
2018-07-10 15:27:37 +1200

    parsync/__main__.py (main): Merged argparsing into main()
    Also changed nargs=1 to nargs='?'

--- parsync/__main__.py
+++ parsync/__main__.py
@@ -4,99 +4,98 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(
-    description='A wrapper for rsync.')
-parser.add_argument(
-    'source',
-    metavar='SRC',
-    type=str,
-    nargs=1,
-    # required=True,
-    help='Source for rsync')
-parser.add_argument(
-    'destination',
-    metavar='DEST',
-    type=str,
-    nargs=1,
-    # required=True,
-    help='Destination for rsync')
-parser.add_argument(
-    '-m', '--main-file',
-    metavar='MAIN_FILE',
-    type=str,
-    nargs=1,
-    help='Main list of files to sync')
-parser.add_argument(
-    '-i', '--ignore-file',
-    metavar='IGNORE_FILE',
-    type=str,
-    nargs=1,
-    help='Ignore list of files')
-parser.add_argument(
-    '-p', '--package',
-    action='store_true',
-    help='Create a directory in DEST for packaged files.')
-parser.add_argument(
-    '-o', '--orphans',
-    action='store_true',
-    help='Create a directory in DEST for orphaned files.\n' +
-    '(files without packages)')
-parser.add_argument(
-    '-D', '--package-dir',
-    metavar='PKG_DIR',
-    type=str,
-    nargs=1,
-    help='Package directory of package file lists.')
-parser.add_argument(
-    '-x', '--main-exec',
-    metavar='MAIN_EXEC',
-    type=str,
-    nargs=1,
-    help='Use MAIN_EXEC to produce a file list.')
-parser.add_argument(
-    '-y', '--ignore-exec',
-    metavar='IGNORE_EXEC',
-    type=str,
-    nargs=1,
-    help='Use IGNORE_EXEC to produce an ignore list.')
-parser.add_argument(
-    '-e', '--package-exec',
-    metavar='PKG_EXEC',
-    type=str,
-    nargs=1,
-    help='Use PKG_EXEC to produce a file list for each package,\n' +
-    '{} is replaced with the package name.')
-parser.add_argument(
-    '-L', '--package-list-exec',
-    metavar="PKG_LIST_EXEC",
-    type=str,
-    nargs=1,
-    help='Use PKG_LIST_EXEC to produce a list of packages.')
-parser.add_argument(
-    '-r', '--alt-root',
-    metavar='ALT_ROOT',
-    type=str,
-    nargs=1,
-    help='Use alternative root, instead of SRC for filelists.')
-parser.add_argument(
-    '-A', '--rsync-args',
-    metavar='RSYNC_ARGS',
-    type=str,
-    nargs=1,
-    help='Use these arguments with rsync.')
-parser.add_argument(
-    '-B', '--rsync-bin',
-    metavar='RSYNC_BIN',
-    type=str,
-    nargs=1,
-    help='Specify an alternative path to rsync binary.')
-
-
-parser.parse_args()
-
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        description='A wrapper for rsync.')
+    parser.add_argument(
+        'source',
+        metavar='SRC',
+        type=str,
+        nargs='?',
+        # required=True,
+        help='Source for rsync')
+    parser.add_argument(
+        'destination',
+        metavar='DEST',
+        type=str,
+        nargs='?',
+        # required=True,
+        help='Destination for rsync')
+    parser.add_argument(
+        '-m', '--main-file',
+        metavar='MAIN_FILE',
+        type=str,
+        nargs='?',
+        help='Main list of files to sync')
+    parser.add_argument(
+        '-i', '--ignore-file',
+        metavar='IGNORE_FILE',
+        type=str,
+        nargs='?',
+        help='Ignore list of files')
+    parser.add_argument(
+        '-p', '--package',
+        action='store_true',
+        help='Create a directory in DEST for packaged files.')
+    parser.add_argument(
+        '-o', '--orphans',
+        action='store_true',
+        help='Create a directory in DEST for orphaned files.\n' +
+        '(files without packages)')
+    parser.add_argument(
+        '-D', '--package-dir',
+        metavar='PKG_DIR',
+        type=str,
+        nargs='?',
+        help='Package directory of package file lists.')
+    parser.add_argument(
+        '-x', '--main-exec',
+        metavar='MAIN_EXEC',
+        type=str,
+        nargs='?',
+        help='Use MAIN_EXEC to produce a file list.')
+    parser.add_argument(
+        '-y', '--ignore-exec',
+        metavar='IGNORE_EXEC',
+        type=str,
+        nargs='?',
+        help='Use IGNORE_EXEC to produce an ignore list.')
+    parser.add_argument(
+        '-e', '--package-exec',
+        metavar='PKG_EXEC',
+        type=str,
+        nargs='?',
+        help='Use PKG_EXEC to produce a file list for each package,\n' +
+        '{} is replaced with the package name.')
+    parser.add_argument(
+        '-L', '--package-list-exec',
+        metavar="PKG_LIST_EXEC",
+        type=str,
+        nargs='?',
+        help='Use PKG_LIST_EXEC to produce a list of packages.')
+    parser.add_argument(
+        '-r', '--alt-root',
+        metavar='ALT_ROOT',
+        type=str,
+        nargs='?',
+        help='Use alternative root, instead of SRC for filelists.')
+    parser.add_argument(
+        '-A', '--rsync-args',
+        metavar='RSYNC_ARGS',
+        type=str,
+        nargs='?',
+        help='Use these arguments with rsync.')
+    parser.add_argument(
+        '-B', '--rsync-bin',
+        metavar='RSYNC_BIN',
+        type=str,
+        nargs='?',
+        help='Specify an alternative path to rsync binary.')
+
+    # parser.parse_args()
+
+    print(parser.parse_args())
 
 
 if __name__ == '__main__':
